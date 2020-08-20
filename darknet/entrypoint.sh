9c7191bfbd227b01c1ad30aed72f43380870cec4
btalberg <ben.talberg@gmail.com>
2020-08-20 08:40:10 -0500

    capture modified cfg and training metric chart before/after training

--- darknet/entrypoint.sh
+++ darknet/entrypoint.sh
@@ -65,14 +65,16 @@ YOLO_VERSION=${PARAM_ARRAY[0]}
 
 if [ "${YOLO_VERSION}" == 'yolov3' ]; then
   yolo_input="./cfg/yolov3-spp.train.cfg ./build/darknet/x64/yolov3-spp-pretrained.conv.113"
-  yolo_cfg_path="./cfg/yolov3-spp.train.cfg"
+  yolo_cfg_name="yolov3-spp.train.cfg"
 elif [ "${YOLO_VERSION}" == 'yolov4' ]; then
   yolo_input="./cfg/yolov4.cfg ./build/darknet/x64/yolov4-pretrained.conv.161"
-  yolo_cfg_path="./cfg/yolov4.cfg"
+  yolo_cfg_name="yolov4.cfg"
 else
   echo "Error: Arg1 should specify yolov3 | yolov4"
   exit 0
 fi
+
+yolo_cfg_path="./cfg/${yolo_cfg_name}"
 
 # Compute step size from max_batch
 step_80=$(printf "%.0f\n" $(echo "${MAX_BATCHES}*.8" | bc))
@@ -91,4 +93,9 @@ sed -i -E "s/steps.*=.*[[:digit:]]+/steps=${steps}/g" "${yolo_cfg_path}"
 sed -i -E "s/filters=255/filters=${class_filters}/g" "${yolo_cfg_path}"
 sed -i -E "s/classes.*=.*[[:digit:]]+/classes=${classes}/g" "${yolo_cfg_path}"
 
+cp "${yolo_cfg_path}" ./backup/${yolo_cfg_name}
+
 exec /build/darknet/darknet detector train data/wf/wf_yolo.data ${yolo_input} -dont_show -map -gpus ${GPUS}
+
+cp chart.png ./backup/chart.png
+
