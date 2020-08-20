e69e017051367730c521e1cab7216ca108e953a4
btalberg <ben.talberg@gmail.com>
2020-08-19 23:41:34 -0500

    add more customizations when starting training from dockerfile, add python script to help compute COCO based map from yolo models

--- darknet/entrypoint.sh
+++ darknet/entrypoint.sh
@@ -6,6 +6,7 @@ PARAMS=""
 GPUS=0
 SUBDIVISIONS=64
 IMG_SIZE=832
+MAX_BATCHES=3500
 while (( "$#" )); do
   case "$1" in
     --gpus)
@@ -35,6 +36,15 @@ while (( "$#" )); do
         exit 1
       fi
       ;;
+    --max-batches)
+      if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+        MAX_BATCHES=$2
+        shift 2
+      else
+        echo "Error: Argument for $1 is missing" >&2
+        exit 1
+      fi
+      ;;
     -*|--*=) # unsupported flags
       echo "Error: Unsupported flag $1" >&2
       exit 1
@@ -46,13 +56,17 @@ while (( "$#" )); do
   esac
 done
 
+if ! command -v bc &> /dev/null; then
+    apt install bc -y
+fi
+
 PARAM_ARRAY=($PARAMS)
 YOLO_VERSION=${PARAM_ARRAY[0]}
 
-if [ "${YOLO_VERSION}" = 'yolov3' ]; then
+if [ "${YOLO_VERSION}" == 'yolov3' ]; then
   yolo_input="./cfg/yolov3-spp.train.cfg ./build/darknet/x64/yolov3-spp-pretrained.conv.113"
   yolo_cfg_path="./cfg/yolov3-spp.train.cfg"
-elif [ "${YOLO_VERSION}" = 'yolov4' ]; then
+elif [ "${YOLO_VERSION}" == 'yolov4' ]; then
   yolo_input="./cfg/yolov4.cfg ./build/darknet/x64/yolov4-pretrained.conv.161"
   yolo_cfg_path="./cfg/yolov4.cfg"
 else
@@ -60,8 +74,21 @@ else
   exit 0
 fi
 
-sed -i -E "s/subdivisions=[[:digit:]]+/subdivisions=${SUBDIVISIONS}/g" "${yolo_cfg_path}"
-sed -i -E "s/width=[[:digit:]]+/width=${IMG_SIZE}/g" "${yolo_cfg_path}"
-sed -i -E "s/height=[[:digit:]]+/height=${IMG_SIZE}/g" "${yolo_cfg_path}"
+# Compute step size from max_batch
+step_80=$(printf "%.0f\n" $(echo "${MAX_BATCHES}*.8" | bc))
+step_90=$(printf "%.0f\n" $(echo "${MAX_BATCHES}*.9" | bc))
+steps="${step_80},${step_90}"
+
+# Set the convolution layer filter size based on # of classes
+classes=$(cat data/wf/wf_yolo.data | awk '/classes.*/ {split($0,x,"="); print x[2]}')
+class_filters=$(echo "(${classes}+5)*3" | bc)
+
+sed -i -E "s/subdivisions.*=.*[[:digit:]]+/subdivisions=${SUBDIVISIONS}/g" "${yolo_cfg_path}"
+sed -i -E "s/width.*=.*[[:digit:]]+/width=${IMG_SIZE}/g" "${yolo_cfg_path}"
+sed -i -E "s/height.*=.*[[:digit:]]+/height=${IMG_SIZE}/g" "${yolo_cfg_path}"
+sed -i -E "s/max_batches.*=.*[[:digit:]]+/max_batches=${MAX_BATCHES}/g" "${yolo_cfg_path}"
+sed -i -E "s/steps.*=.*[[:digit:]]+/steps=${steps}/g" "${yolo_cfg_path}"
+sed -i -E "s/filters=255/filters=${class_filters}/g" "${yolo_cfg_path}"
+sed -i -E "s/classes.*=.*[[:digit:]]+/classes=${classes}/g" "${yolo_cfg_path}"
 
 exec /build/darknet/darknet detector train data/wf/wf_yolo.data ${yolo_input} -dont_show -map -gpus ${GPUS}
--- scripts/darknet-training-setup.sh
+++ scripts/darknet-training-setup.sh
@@ -4,8 +4,11 @@ pip3 install boto3
 
 cd /build/darknet
 
-if [ ! -f "./wf_data/wf-yolo-v0.1.tgz" ]; then
-    echo "Downloading training data..."
-    python3 /scripts/s3_download.py --s3-file-url s3://wf-sagemaker-us-east-2/final_training_sets/wf-yolo-v0.1.tar.gz --dest /build/darknet/wf_data/
-    tar xvf ./wf_data/wf-yolo-v0.1.tar.gz -C ./wf_data
+url=$1
+filename_with_extension="${url##*/}"
+
+if [ ! -f "${filename_with_extension}" ]; then
+    echo "Downloading ${filename_with_extension} training data..."
+    python3 /scripts/s3_download.py --s3-file-url ${url} --dest /build/darknet/wf_data/
+    tar xvf ./wf_data/${filename_with_extension} -C ./wf_data
 fi
